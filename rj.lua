c0309ad71208e114548ae4b19eca27b1b719738b
YeemiRouth <yeemirouth@gmail.com>
2026-02-06 15:57:56 +1100

    Simplify teleport logic for players

--- rj.lua
+++ rj.lua
@@ -3,12 +3,7 @@ print'did u trigger'
 local Players = game.Players
 local TeleportService = game:GetService("TeleportService")
 
-if #Players:GetPlayers() <= 1 then
-            Players.LocalPlayer:Kick("\nRejoining...")
-            wait()
-            TeleportService:Teleport(game.PlaceId, Players.LocalPlayer)
-        else
-            TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId, Players.LocalPlayer)
-        end
+Players.LocalPlayer:Kick("\nRejoining...")
+TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId, Players.LocalPlayer)
 
 queue_on_teleport(game:httpget("https://raw.githubusercontent.com/Laamy/app-releases/refs/heads/main/rj.lua"))
