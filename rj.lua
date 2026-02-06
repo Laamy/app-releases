011fe4991d2ef50e0405802392af029a856e179a
YeemiRouth <yeemirouth@gmail.com>
2026-02-06 15:58:42 +1100

    Update rj.lua

--- rj.lua
+++ rj.lua
@@ -6,4 +6,4 @@ local TeleportService = game:GetService("TeleportService")
 Players.LocalPlayer:Kick("\nRejoining...")
 TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId, Players.LocalPlayer)
 
-queue_on_teleport(game:httpget("https://raw.githubusercontent.com/Laamy/app-releases/refs/heads/main/rj.lua"))
+queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/Laamy/app-releases/refs/heads/main/rj.lua"))
